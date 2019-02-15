8955f59c9f62d8dfb9fff5e2242b642678725011
vinian <willian.wnh@gmail.com>
2019-02-15 11:23:48 -0500

    customize rust

--- lisp/init-locales.el
+++ lisp/init-locales.el
@@ -21,5 +21,34 @@
   (set-selection-coding-system (if (eq system-type 'windows-nt) 'utf-16-le 'utf-8))
   (prefer-coding-system 'utf-8))
 
+(set-face-attribute 'default nil
+                    :family "Monaco" :height 150 :weight 'normal)
+
+(desktop-save-mode t)
+
+;; 外观
+(setq frame-title-format "willian.wnh@gmail.com")
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq time-stamp-start "最后更新时间:[   ]+\\\\?")
+(setq time-stamp-end: "\n")
+(setq time-stamp-format: "%:y年%:m月%:d日")
+
+;; 高亮显示拷贝区域
+(transient-mark-mode t)
+
+;; 交互式粘贴
+(setq x-select-enable-clipboard t)
+
+;; 语法高亮
+(global-font-lock-mode 1)
+
+;; 自动显示图片
+(auto-image-file-mode)
+
+;; 光标靠近指针时，指针自动让开
+(mouse-avoidance-mode 'animate)
+
 (provide 'init-locales)
 ;;; init-locales.el ends here
